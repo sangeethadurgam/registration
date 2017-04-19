3355293c627cacb601202bab1ff20d6020862aac
sangeetha <sangeetha@192.168.1.27>
2017-04-19 13:17:48 +0530

    registration

---
+++ registration/src/main/java/registration/Registration.java
@@ -0,0 +1,57 @@
+package registration;
+
+
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.sql.*;
+public class Registration extends HttpServlet {
+
+	
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response){
+		try{
+			System.out.println("hi");
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+	String fn=request.getParameter("fname");   
+	String ln=request.getParameter("lname");
+	String un=request.getParameter("uname");
+	String pw=request.getParameter("pword");
+	//Load the driver
+	System.out.println("database");
+		Class.forName("org.h2.Driver");
+		System.out.println("hi");
+		//Establish the connection object
+		Connection con=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test","sa","");
+		//Execute the query
+		String qry="insert into REGISTRATION values(?,?,?,?)";
+		PreparedStatement pst=con.prepareStatement(qry);
+		pst.setString(1, fn);
+		pst.setString(2, ln);
+		pst.setString(3, un);
+		pst.setString(4, pw);
+		
+		int i=pst.executeUpdate();
+		if(i>0)
+		{
+		response.sendRedirect("index.jsp");
+		}
+		else
+		{
+			out.println("Transaction failed");
+		}
+	
+	}
+	
+	catch(Exception e)
+	{e.printStackTrace();}
+		
+	}
+	
+}
+
+
+
---
+++ registration/src/main/webapp/hello.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href = "Login.html">Login here</a>
+</body>
+</html>
